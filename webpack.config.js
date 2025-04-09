257390f13afacb405da8ea2752d4b97462e5f319
coding <cd62970@gmail.com>
2025-04-09 20:08:17 +0300

    deployment in vercel

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,37 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin'); // Optional for path mapping
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { plugin } = require('mongoose');
+/** @format */
 
-module.exports =
-  function (options, webpack) {
-    return {
-      ...options,
-      entry: [
-        options.entry,
-      ],
-      target: 'node',
-      output: {
-        filename: 'main.js',
-        path:
-          path
-            .resolve(
-              __dirname,
-              'dist',
-            ),
-      },
-      externals: [
-        nodeExternals(),
-      ],
-      plugins: [
-        new CopyWebpackPlugin(
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const TsConfigPathsPlugin = require("tsconfig-paths-webpack-plugin"); // Optional for path mapping
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { plugin } = require("mongoose");
+
+module.exports = function (options, webpack) {
+  return {
+    ...options,
+    entry: [options.entry],
+    target: "node",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    externals: [nodeExternals()],
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
           {
-            patterns: [
-              {
-                from:
-                  path
-                    .resolve(
-                      __dirname,
-                      'src/resources/email-templates',
-                    ),
-                to:
-                  path
-                    .resolve(
-                      __dirname,
-                      'dist/resources/email-templates',
-                    ),
-              },
-            ],
+            from: path.resolve(__dirname, "src/resources/email-templates"),
+            to: path.resolve(__dirname, "dist/resources/email-templates"),
           },
-        ),
-      ],
-      resolve: {
-        extensions: [
-          '.ts',
-          '.js',
         ],
-        plugins: [
-          new TsConfigPathsPlugin(),
-        ],
-      },
-      optimization: {
-        minimize: false,
-      },
-    };
+      }),
+    ],
+    resolve: {
+      extensions: [".ts", ".js"],
+      plugins: [new TsConfigPathsPlugin()],
+    },
+    optimization: {
+      minimize: false,
+    },
   };
+};
